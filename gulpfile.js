90821dc76c7cb9e4cd22ac209f0413754a17a8da
lars <larsvo@gmail.com>
2020-06-16 13:18:53 +0200

    gulp wip

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browsersync = require('browsersync').create(); 
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browsersync = require('browser-sync').create(); 
 
 //compile scss to css
+
+function style() {
+    //1 where is my css file
+    return gulp.src('./scss/**/*.scss')
+    //2 pass scss compiler
+    .pipe(sass())
+    //3 where t save css
+    .pipe(gulp.dest('./css'));
+}
+
+exports.style = style;
